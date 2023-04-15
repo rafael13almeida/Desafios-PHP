cb3ce6c8c6cc38b119c5e244a3f0308f70031e04
Rafael Almeida <rafael13almeida@gmail.com>
2023-04-15 11:07:25 -0300

    Adicionando lógica ao desafio 07

--- desafio07/index.php
+++ desafio07/index.php
@@ -10,12 +10,25 @@
 <body>
     <main>
         <h1>Informe seu Salário</h1>
+        <?php 
+            $salario = $_POST['salario'];
+            $minimo = 1380;
+        ?>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="salario">Salário (R$)</label>
-            <input type="number" name="salario" id="salario" min="0" value="0" require>
+            <input type="number" name="salario" id="salario" min="0" step="0.01" value="0" require>
             <p>Considerando o salário mínimo de R$1.380</p>
             <input type="submit" value="Calcular">
         </form>
     </main>
+
+    <section>
+        <h2>Resposta</h2>
+        <?php 
+        $numSalario = (int) ($salario / $minimo);
+        $restoSalario = $salario % $minimo;
+        echo "Quem recebe um salário de R$",number_format($salario, 2, ",", ".")." ganha <strong>".$numSalario." salário(s) mínimo(s)</srtong>  + <strong>R$".number_format($restoSalario, 2, ",", ".")."</strong>.";
+        ?>
+    </section>
 </body>
 </html>
