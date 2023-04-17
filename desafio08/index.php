8d786e110da285f8377929fc6ea61e354e3bfaa0
Rafael Almeida <rafael13almeida@gmail.com>
2023-04-17 18:43:31 -0300

    Preparando desafio 08

---
+++ desafio08/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informe um Número</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Infome um Número</h1>
+
+        <form action="" method="post">
+            <label for="numero">Número:</label>
+            <input type="number" min="0" require>
+            <input type="submit" value="Calcular Raízes">
+        </form>
+    </main>
+
+    <section>
+        <h2>Resultado Final</h2>
+    </section>
+</body>
+</html>
