7cdb28135bcc3f70bc1223e831784cc7f6d0c2e2
Rafael Almeida <rafael13almeida@gmail.com>
2023-04-18 21:14:48 -0300

    Adicionando lógica nos calculos de média

--- desafio09/index.php
+++ desafio09/index.php
@@ -19,25 +19,34 @@
         ?>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="valor1">1º Valor</label>
-            <input type="number" name="valor1" id="valor1">
+            <input type="number" name="valor1" id="valor1" value="<?=$primValor?>">
             <label for="peso1">1º Peso</label>
-            <input type="number" name="peso1" id="peso1">
+            <input type="number" name="peso1" id="peso1" value="<?=$primPeso?>">
             <label for="valor2">2º Valor</label>
-            <input type="number" name="valor2" id="valor2">
+            <input type="number" name="valor2" id="valor2" value="<?=$segValor?>">
             <label for="peso2">2º Peso</label>
-            <input type="number" name="peso2" id="peso2">
+            <input type="number" name="peso2" id="peso2" value="<?=$segPeso?>">
             <input type="submit" value="Calcular Médias">
         </form>
     </main>
     <?php 
-        
+        $mediaSimples = ($primValor + $segValor) / 2;
+        $primeiraMedia = ($primValor * $primPeso + $segValor * $segPeso);
+        $mediaFinal = $primeiraMedia / ($primPeso + $segPeso);
     ?>
 
     <section>
         <h2>Cálculo das Médias</h2>
-        <ul>
 
-        </ul>
+            <?php 
+                echo 
+                "<p>Analisando os valores entre $primValor e $segValor: </p>".
+                "<ul>".
+                    "<li>A <strong>Média Aritmética Simples</strong> entre os valores é igual a ".number_format($mediaSimples, 2, ",", ".")."</li>".
+                    "<li>A <strong>Média Aritmética Ponderada</strong> entre os valores é igual a ".number_format($mediaFinal, 2, ",", ".")."</li>".
+                "</ul>";
+            ?>
+
     </section>
 </body>
 </html>
