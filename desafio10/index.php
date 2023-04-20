c00ce93a43021cae9ec77dc3aef007b1ca19675a
Rafael Almeida <rafael13almeida@gmail.com>
2023-04-19 21:23:45 -0300

    Iniciando lógica nos calculos

--- desafio10/index.php
+++ desafio10/index.php
@@ -10,17 +10,30 @@
 <body>
     <main>
         <h1>Calculando sua Idade</h1>
-        <form action="" method="post">
+
+        <?php 
+
+            $anoNascimento = date($_POST['anoNascimento']) ?? 2023;
+            $anoCuriosidade = date($_POST['anoCuriosidade']) ?? 2023;
+        ?>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="ano de nascimento">Digite o ano em que você nasceu:</label>
-            <input type="number" name="anoNascimento" id="anoNascimento" min="1900" max="2100">
+            <input type="number" name="anoNascimento" id="anoNascimento">
             <label for="ano de curiosidade">Quer saber sua idade em que ano?</label>
-            <input type="number" name="anoCuriosidade" id="anoCuriosidade" min="1900" max="2100">
+            <input type="number" name="anoCuriosidade" id="anoCuriosidade">
             <input type="submit" value="Qual será minha idade?">
         </form>
     </main>
 
     <section>
         <h2>Resultado</h2>
+        <?php 
+            
+            $idade = abs($anoCuriosidade - $anoNascimento);
+            //Fazer validação para idade inferior a 0
+            var_dump($anoNascimento, $anoCuriosidade, $idade); die;
+            echo "<p>$idade</p>";
+        ?>
     </section>
 </body>
 </html>
