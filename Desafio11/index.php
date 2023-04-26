93be20a66710ecbfa2de4648e66c9cfaeb5b0f89
Rafael Almeida <rafael13almeida@gmail.com>
2023-04-25 21:26:06 -0300

    Melhorando exibição no type range

--- Desafio11/index.php
+++ Desafio11/index.php
@@ -13,15 +13,26 @@
 
         <?php 
             $preco =    $_POST['preco'] ?? 0;
-            $reajuste = $_POST['reajuste'] ??0;
+            $reajuste = $_POST['reajuste'] ?? 0;
+            
         ?>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="preço">Informe o valor do produto (R$):</label>
-            <input type="number" name="preco" id="preco" step="0.01" require>
+            <input type="number" name="preco" id="preco" step="0.01" value="<?=$preco?>" require>
             <label for="reajuste">Qual será o percentual de reajuste? <strong>(%<?=$reajuste?>)</strong></label>
-            <input type="range" name="reajuste" id="reajuste" min="0" max="100" value="<?=$reajuste?>">
+            <input type="range" name="reajuste" id="reajuste" min="0" max="100" step="1" onchange="this.form.submit();" value="<?=$reajuste?>">
             <input type="submit" value="Reajustar">
         </form>
     </main>
+
+    <section>
+        <h2>Resultado do Reajuste</h2>
+        <?php 
+            $valorPercentual = $reajuste / 100;
+            $valorReajuste = $preco + ($preco * $valorPercentual);
+            echo "<p>O produto que custava R$".number_format($preco, 2, ",", ".")." com ".$reajuste."% de aumento<strong> vai passar a custar R$".number_format($valorReajuste, 2, ",", ".") ."</strong></p>";           
+        ?>
+    </section>
+
 </body>
 </html>
