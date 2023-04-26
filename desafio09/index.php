c6523a249f1bb3c116fb0317b41840d73553fef0
Rafael Almeida <rafael13almeida@gmail.com>
2023-04-25 21:25:05 -0300

    Finalizando lógica

--- desafio09/index.php
+++ desafio09/index.php
@@ -12,27 +12,26 @@
         <h1>Médias Aritiméticas</h1>
 
         <?php   
-            $primValor = $_POST['valor1'] ?? 0;
-            $primPeso  = $_POST['peso1']  ?? 0;
-            $segValor  = $_POST['valor2'] ?? 0;
-            $segPeso   = $_POST['peso2']  ?? 0;
+            $primValor = $_POST['valor1'] ?? '';
+            $primPeso  = $_POST['peso1']  ?? '';
+            $segValor  = $_POST['valor2'] ?? '';
+            $segPeso   = $_POST['peso2']  ?? '';
         ?>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="valor1">1º Valor</label>
             <input type="number" name="valor1" id="valor1" value="<?=$primValor?>">
             <label for="peso1">1º Peso</label>
-            <input type="number" name="peso1" id="peso1" value="<?=$primPeso?>">
+            <input type="number" name="peso1" id="peso1" min="1" value="<?=$primPeso?>">
             <label for="valor2">2º Valor</label>
             <input type="number" name="valor2" id="valor2" value="<?=$segValor?>">
             <label for="peso2">2º Peso</label>
-            <input type="number" name="peso2" id="peso2" value="<?=$segPeso?>">
+            <input type="number" name="peso2" id="peso2" min="1" value="<?=$segPeso?>">
             <input type="submit" value="Calcular Médias">
         </form>
     </main>
     <?php 
         $mediaSimples = ($primValor + $segValor) / 2;
-        $primeiraMedia = ($primValor * $primPeso + $segValor * $segPeso);
-        $mediaFinal = $primeiraMedia / ($primPeso + $segPeso);
+        $mediaPonderada = ($primValor * $primPeso + $segValor * $segPeso) / ($primPeso + $segPeso);
     ?>
 
     <section>
@@ -43,7 +42,7 @@
                 "<p>Analisando os valores entre $primValor e $segValor: </p>".
                 "<ul>".
                     "<li>A <strong>Média Aritmética Simples</strong> entre os valores é igual a ".number_format($mediaSimples, 2, ",", ".")."</li>".
-                    "<li>A <strong>Média Aritmética Ponderada</strong> entre os valores é igual a ".number_format($mediaFinal, 2, ",", ".")."</li>".
+                    "<li>A <strong>Média Aritmética Ponderada</strong> entre os valores é igual a ".number_format($mediaPonderada, 2, ",", ".")."</li>".
                 "</ul>";
             ?>
 
